81de2581fc0d64ff63dedbf0c54bcfae7199007e
Weiner Oliveira <woliveira82@gmail.com>
2024-08-11 15:46:10 +0100

    refact: code with process func

--- scenes/card/card.gd
+++ scenes/card/card.gd
@@ -1,22 +1,21 @@
-extends Button
+extends Control
 
-var following_mouse: bool = false
+var mouse_hover: bool = false
 var angle_x_max: float = 0.01
 var angle_y_max: float = 0.01
 
 var tween_rot: Tween
 
+
 @onready var card_texture = $CardTexture
 
 
-func _on_gui_input(event):
-	
-	if following_mouse: return
-	if not event is InputEventMouseMotion: return
-	
+func _process(_delta):
+	if mouse_hover: _rotate_card()
+
+
+func _rotate_card():
 	var mouse_pos: Vector2 = get_local_mouse_position()
-	print("Mouse: ", mouse_pos)
-	print("Card: ", position + size)
 	var diff: Vector2 = mouse_pos
 	
 	var lerp_val_x: float = remap(mouse_pos.x, 0.0, size.x, 0, 1)
@@ -29,10 +28,19 @@ func _on_gui_input(event):
 	card_texture.material.set_shader_parameter("y_rot", rot_x)
 
 
-func _on_mouse_exited():
+func _rotate_card_back():
 	if tween_rot and tween_rot.is_running():
 		tween_rot.kill()
-		
+	
 	tween_rot = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK).set_parallel(true)
 	tween_rot.tween_property(card_texture.material, "shader_parameter/x_rot", 0.0, 0.5)
 	tween_rot.tween_property(card_texture.material, "shader_parameter/y_rot", 0.0, 0.5)
+
+
+func _on_mouse_entered():
+	mouse_hover = true
+
+
+func _on_mouse_exited():
+	mouse_hover = false
+	_rotate_card_back()
