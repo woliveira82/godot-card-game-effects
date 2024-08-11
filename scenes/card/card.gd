26b4fd3d8b953477f70807469e700e9198ffadd6
Weiner Oliveira <woliveira82@gmail.com>
2024-08-11 15:47:10 +0100

    feat: add drag card

--- scenes/card/card.gd
+++ scenes/card/card.gd
@@ -1,10 +1,12 @@
 extends Control
 
 var mouse_hover: bool = false
+var mouse_drag: bool = false
 var angle_x_max: float = 0.01
 var angle_y_max: float = 0.01
 
 var tween_rot: Tween
+var tween_handle: Tween
 
 
 @onready var card_texture = $CardTexture
@@ -12,6 +14,7 @@ var tween_rot: Tween
 
 func _process(_delta):
 	if mouse_hover: _rotate_card()
+	if mouse_drag: _drag_card()
 
 
 func _rotate_card():
@@ -35,6 +38,17 @@ func _rotate_card_back():
 	tween_rot = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK).set_parallel(true)
 	tween_rot.tween_property(card_texture.material, "shader_parameter/x_rot", 0.0, 0.5)
 	tween_rot.tween_property(card_texture.material, "shader_parameter/y_rot", 0.0, 0.5)
+
+
+func _drag_card():
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	global_position = mouse_pos
+
+
+func _on_gui_input(event: InputEvent):
+	if not event is InputEventMouseButton: return
+	if event.button_index != MOUSE_BUTTON_LEFT: return
+	mouse_drag = event.is_pressed()
 
 
 func _on_mouse_entered():
